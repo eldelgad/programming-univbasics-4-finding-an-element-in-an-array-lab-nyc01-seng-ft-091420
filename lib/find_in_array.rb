d78c8e3ffc788cae927bb524743f39a888c6d6e4
Elias Delgado <evdelgado99@gmail.com>
2020-08-28 22:41:58 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,10 +3,9 @@ def find_element_index(array, value_to_find)
   counter = 0
   
   while counter < array.length do
-
     puts array[counter] == value_to_find
     counter += 1
-    
   end
+  return counter
 end
     
